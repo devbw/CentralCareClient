3a4f30e2a6803bd5bb739f807b6e42c16cc4bde8
devbw <wullaertbrian@gmail.com>
2021-10-15 15:03:36 +0200

    add circle and circle's animation, and some new stuff, see to check flex-wrap on circles for responsive

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -13,7 +13,21 @@ const Accueil = () => {
         <button>INSCRIPTION</button>
       </div>
       <div className="home-trapezoid">
-        
+        <h2>Comment fonctionne <br/> la plateforme ?</h2>
+        <div className="home-benefits">
+          <div className="benefits">
+            <div className="circle"><span>1</span></div>
+            <p>Je m'inscris sur la plateforme</p>
+          </div>
+          <div className="benefits">
+            <div className="circle"><span>2</span></div>
+            <p>Je poste ou réponds à un annonce</p>
+          </div>
+          <div className="benefits">
+            <div className="circle"><span>3</span></div>
+            <p>Je trouve mon intervenant ou bénéficiaire</p>
+          </div>
+        </div>
       </div>
     </div>
   );
