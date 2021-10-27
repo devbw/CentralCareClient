5a8e926c923afa3bce1c2c64ace738ed2784b9f8
Pierrick Rancoeur <pierre.ankh@gmail.com>
2021-10-27 13:07:55 +0200

    fix affichage annonces

--- src/components/Adverts/Adverts.js
+++ src/components/Adverts/Adverts.js
@@ -1,35 +1,42 @@
 import React from 'react';
 
 const Adverts = (props) => {
-  return (
-    <div className="adverts">
-      <div className="top-adverts">
-        <span>{props.user_id}</span>
-        <i className="fas fa-ellipsis-h"></i>
-      </div>
-      <h3>{props.title}</h3>
-      <div className="category-adverts">
-        <p>Catégorie :</p>
-        <p>{props.category}</p>
-      </div>
-      <div>
-        <p>Type :</p>
-        <p>{props.type}</p>
-      </div>
-      <div>
-        {props.content}
-      </div>
-      <div className="actions">
-        <div>
-          <button>Voir tout</button>
-          <button>Répondre</button>
-        </div>
-        <div>
-          <i className="far fa-heart"></i>
-        </div>
-      </div>
-    </div>
-  );
+	const { adverts } = props;
+
+	if (adverts.length > 0) {
+		return adverts.map((advert, index) => {
+			console.log(advert);
+			return (
+				<div className='adverts' key={advert.id}>
+					<div className='top-adverts'>
+						<span>{advert.user_id}</span>
+						<i className='fas fa-ellipsis-h'></i>
+					</div>
+					<h3>{advert.title}</h3>
+					<div className='category-adverts'>
+						<p>Catégorie :</p>
+						<p>{advert.category}</p>
+					</div>
+					<div>
+						<p>Type :</p>
+						<p>{advert.type}</p>
+					</div>
+					<div>{advert.content}</div>
+					<div className='actions'>
+						<div>
+							<button>Voir tout</button>
+							<button>Répondre</button>
+						</div>
+						<div>
+							<i className='far fa-heart'></i>
+						</div>
+					</div>
+				</div>
+			);
+		});
+	} else {
+		return <p>Aucune annonce pour le moment</p>;
+	}
 };
 
 export default Adverts;
