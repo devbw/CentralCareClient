4b268818e089d5c33d43753b217ef4037c223369
devbw <wullaertbrian@gmail.com>
2021-11-01 22:45:07 +0100

    begin account page

--- src/pages/Compte.js
+++ src/pages/Compte.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from '../components/Navigation';
+import jwt_decode from 'jwt-decode';
+import Cookies from 'js-cookie'
+import axios from 'axios';
 
 const Compte = () => {
+
+  useEffect(() => {
+    retrieveUser();
+  })
+
+  const getCookie = () => {
+    return Cookies.get('token');
+  }
+  const decodeToken = () => {
+    let decoded = jwt_decode(getCookie());
+    return decoded.id
+  }
+  const retrieveUser = () => {
+    axios.get(`https://centralcare.srpweb.fr/users/${decodeToken()}`)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
   return (
     <div>
       <Navigation />
