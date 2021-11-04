737b749f41cbce96369038a66a45a164b3b44828
devbw <wullaertbrian@gmail.com>
2021-11-04 23:09:29 +0100

    begin account page

--- src/pages/Compte.js
+++ src/pages/Compte.js
@@ -1,10 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Navigation from '../components/Navigation';
 import jwt_decode from 'jwt-decode';
 import Cookies from 'js-cookie'
 import axios from 'axios';
 
 const Compte = () => {
+
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
     retrieveUser();
@@ -17,18 +19,28 @@ const Compte = () => {
     let decoded = jwt_decode(getCookie());
     return decoded.id
   }
+  const retrieveJWT = () => {
+    return getCookie();
+  }
   const retrieveUser = () => {
-    axios.get(`https://centralcare.srpweb.fr/users/${decodeToken()}`)
+    axios.get(`https://centralcare.srpweb.fr/users/${decodeToken()}`, {
+      headers: {
+        Authorization: `Bearer ${retrieveJWT()}`
+      }
+    })
     .then((res) => {
-      console.log(res.data)
+      console.log(res.data.username)
+      setUsername(res.data.username)
     })
     .catch((err) => {
       console.log(err);
     })
   }
   return (
-    <div>
+    <div className="account-container">
       <Navigation />
+      <h1>Bienvenue, {username}</h1>
+      <p>Retrouvez ici toutes les informations liées à votre compte</p>
     </div>
   );
 };
