425caac62c7c820e2029f32eb7e9af403e2c4831
devbw <wullaertbrian@gmail.com>
2021-11-07 22:40:20 +0100

    add style to account buttons

--- src/pages/Compte.js
+++ src/pages/Compte.js
@@ -43,35 +43,35 @@ const Compte = () => {
         <h1>Bienvenue, {userInfos.username}</h1>
         <p>Retrouvez ici toutes les informations liées à votre compte</p>
       </div>
-      <div className="account-block">
+      <div className="account-block fade-in-bottom">
         <div className="title-subblock">
           <i className="fas fa-portrait"></i>
           <h4>Mes informations</h4>
         </div>
         <button>Modifier mes informations</button>
       </div>
-      <div className="account-block">
+      <div className="account-block fade-in-bottom">
         <div className="title-subblock">
           <i className="far fa-clipboard"></i>
           <h4>Mes annonces</h4>
         </div>
         <button>Voir mes annonces</button>
       </div>
-      <div className="account-block">
+      <div className="account-block fade-in-bottom">
         <div className="title-subblock">
           <i className="fas fa-heart"></i>
           <h4>Mes favoris</h4>
         </div>
         <button>Voir mes favoris</button>
       </div>
-      <div className="account-block">
+      <div className="account-block fade-in-bottom">
         <div className="title-subblock">
           <i className="fas fa-shield-alt"></i>
           <h4>Sécurité</h4>
         </div>
         <button>Modifier mot de passe</button>
       </div>
-      <div className="account-block">
+      <div className="account-block fade-in-bottom">
         <div className="title-subblock">
           <i className="fas fa-user-times"></i>
           <h4>Supprimer</h4>
