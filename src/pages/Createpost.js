cadc45642dc57752a45e291eda4531e4d5548bf3
devbw <wullaertbrian@gmail.com>
2021-11-25 23:34:53 +0100

    add some style to post container

--- src/pages/Createpost.js
+++ src/pages/Createpost.js
@@ -3,8 +3,14 @@ import Navigation from '../components/Navigation';
 
 const Createpost = () => {
   return (
-    <div>
+    <div className="create-container">
       <Navigation />
+      <h1>Créez votre annonce</h1>
+      <div className="postform-container">
+        <form>
+          
+        </form>
+      </div>
     </div>
   );
 };
