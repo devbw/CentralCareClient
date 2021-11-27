65edf71055ad37b73b0d88b31fab9a47573094c6
devbw <wullaertbrian@gmail.com>
2021-11-27 23:28:23 +0100

    add range input

--- src/pages/Createpost.js
+++ src/pages/Createpost.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import Navigation from "../components/Navigation";
+import axios from 'axios';
 
 const Createpost = () => {
   const [title, setTitle] = useState("");
   const [postContent, setPostContent] = useState("");
   const [city, setCity] = useState("");
+  const [price, setPrice] = useState("");
+
+  const publishPost = (e) => {
+    e.preventDefault();
+    axios.post('https://www.facebook.com/', {
+      title,
+      postContent,
+      city,
+      price
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
 
   return (
     <div className="create-container">
@@ -22,16 +40,16 @@ const Createpost = () => {
             onChange={(e) => setTitle(e.target.value)}
             required
           />
-          <label htmlFor="postContent">Contenu de l'annonce</label>
-          <input
-            type="text"
-            name="postContent"
-            id="postContent"
-            placeholder="Je recherche..."
+          <label htmlFor="story">Contenu de l'annonce</label>
+          <textarea
+            id="story"
+            name="story"
+            rows="5"
+            cols="33"
+            placeholder="Je propose mes services de..."
             value={postContent}
             onChange={(e) => setPostContent(e.target.value)}
-            required
-          />
+          ></textarea>
           <label htmlFor="city">Ville</label>
           <input
             type="text"
@@ -42,7 +60,7 @@ const Createpost = () => {
             onChange={(e) => setCity(e.target.value)}
             required
           />
-          <label htmlFor="price">Prix €/h</label>
+          <label htmlFor="price">Prix à l'heure</label>
           <input
             type="range"
             name="price"
@@ -50,8 +68,12 @@ const Createpost = () => {
             placeholder="10€/h"
             min="8"
             max="30"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
           />
-          <button type="submit">Poster</button>
+          <span>{price}€/h</span>
+          <button type="submit" onClick={publishPost}>Poster</button>
         </form>
       </div>
     </div>
